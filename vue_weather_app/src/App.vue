ca567f8e43e667f7e1ec6891389baa7f7451f07e
Ingvar <my@example.com>
2025-04-18 20:02:45 +0300

    develop weather api added

--- vue_weather_app/src/App.vue
+++ vue_weather_app/src/App.vue
@@ -1,23 +1,70 @@
 <script>
+    import axios from 'axios'
     export default {
         data() {
+            return {
+                city: "",
+                error: "",
+                info: null
+            }
+        },
+        computed: {
+            cityName() {
+                return "городе " + this.city
+            },
+            showTemp() {
+                return "Сейчас: " + this.info.main.temp
+            },
+            showFeelsLike() {
+                return "Ощущается: " + this.info.main.feels_like
+            },
+            showTempMin() {
+                return "Минимум сегодня: " + this.info.main.temp_min
+            },
+            showTempMax() {
+                return "Максимум сегодня: " + this.info.main.temp_max
+            },
+        },
+        methods: {
+            getWeather() {
+                if(this.city.trim().length < 2) {
+                    this.error = "Некорректно указан город"
+                    return false
+                }
+                else {
+                    this.error = ""
 
+                    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=756c3c4bc3bab24102880c3fbe2b4539`)
+                        .then(res => (this.info = res.data))
+                }
+            }
         }
     }
 </script>
 
 <template>
     <div className="wrapper">
-        <h1>School 21 - Погода</h1>
-        <p>Узнай погоду в твоем городе!</p>
-        <input type="text" placeholder="Укажите город">
-        <button>Узнать погоду</button>
+        <h1>Погода</h1>
+        <p>Узнай погоду в {{ city == "" ? "твоем городе" : cityName }} </p>
+        <div className="input_block">
+            <input type="text" v-on:input="this.city = $event.target.value" placeholder="Укажите город">
+            <button v-if="city != '' "@click="getWeather()">Узнать погоду</button>
+            <button disabled v-else>Введите название города</button>
+        </div>
+        <p className="error">{{ error }}</p>
+        <div className="weather_info" v-if="info != null">
+            <p>{{ showTemp }}</p>
+            <p>{{ showFeelsLike }}</p>
+            <p>{{ showTempMin }}</p>
+            <p>{{ showTempMax }}</p>
+        </div>
     </div>
 </template>
 
 <style scoped>
     .wrapper {
-        background-color: blueviolet;
+        font-family: Arial, Helvetica, sans-serif;
+        background-color: rgb(47, 0, 92);
         width: 900px;
         height: 500px;
         border-radius: 30px;
@@ -27,6 +74,9 @@
     }
     .wrapper p {
         margin-top: 10px;
+    }
+    .input_block {
+        min-height: 100px;
     }
     .wrapper input {
         outline: none;
@@ -56,9 +106,18 @@
         cursor: pointer;
         transition: transform 200ms ease-in;
     }
+    .wrapper button:disabled {
+        cursor: not-allowed;
+        background: grey;
+        transition: none;
+    }
     .wrapper button:hover {
         border-bottom: 3px solid rgb(107, 0, 107);
         background: linear-gradient(110deg,rgba(83, 237, 219, 1) 0%, rgba(222, 92, 255, 1) 50%, rgba(87, 199, 115, 1) 100%);
         transform: translateY(-2px);
+    }
+    .error {
+        margin-top: 50px;
+        color: brown;
     }
 </style>
